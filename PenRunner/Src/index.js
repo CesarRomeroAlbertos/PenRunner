02e8d1acf970e5b64c5d05392cb47ee54bfface2
César Romero Albertos <43405217+CesarRomeroAlbertos@users.noreply.github.com>
2018-10-14 20:09:57 +0200

    Creados archivos index

---
+++ PenRunner/Src/index.js
@@ -0,0 +1,13 @@
+//creamos la variable del juego fijando el tamaño de la ventana
+var game = new Phaser.Game(800, 600, Phaser.AUTO, 'gameDiv')
+  
+  //aquí se añaden las escenas al juego, si queréis añadir alguna más hay que hacerlo aquí
+/*game.state.add('bootState', CatCatcher.bootState)
+game.state.add('preloadState', CatCatcher.preloadState)
+game.state.add('menuState', CatCatcher.menuState)
+game.state.add('levelState', CatCatcher.levelState)
+game.state.add('endingState', CatCatcher.endingState)*/
+
+  //llamamos a la scena boot
+//game.state.start('bootState')
+
