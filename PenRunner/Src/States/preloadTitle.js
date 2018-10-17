910736eadf4fdb8046576ac751228a808c05056f
César Romero Albertos <43405217+CesarRomeroAlbertos@users.noreply.github.com>
2018-10-17 16:34:03 +0200

    Escena títulop con imagen estática
    De momento he puesto el gif pero no está animado, tendré que sustituirlo con un spritesheet. También he añadido un texto y ahora para pasar la escena del título hay que hacer click.

--- PenRunner/Src/States/preloadTitle.js
+++ PenRunner/Src/States/preloadTitle.js
@@ -9,7 +9,9 @@ PenRunner.preloadTitleState.prototype =
 		var text = "Loading...";
     	var style = { font: "65px Arial", fill: "#ffffff", align: "center" };
 
-    	var t = game.add.text(game.world.centerX-150, game.world.centerY-100, text, style);
+		var t = game.add.text(game.world.centerX-150, game.world.centerY-100, text, style);
+		
+		game.load.image('title','Assets/title.gif');
 	},
 	update: function()
 	{
--- PenRunner/Src/States/title.js
+++ PenRunner/Src/States/title.js
@@ -4,11 +4,25 @@ PenRunner.titleState = function(game) {
 
 PenRunner.titleState.prototype =
 {
-	create: function(){},
+	create: function()
+	{
+		game.stage.backgroundColor = "#FFFFFF";
+		s = game.add.image(0,40,'title')
+		var text = "CLICK TO START";
+		var style = { font: "32px Lucida Console", fill: "#000000", align: "center", fontWeight:"bold"};
+		
+
+		var t = game.add.text(game.world.centerX-128, game.world.centerY+220, text, style);
+
+		//t.stroke = '#000000';
+		//t.strokeThickness = 1;
+		
+		game.input.mouse.capture = true;
+	},
 	update: function()
 	{
-
-		game.state.start('preloadMenuState');
+		if(game.input.activePointer.leftButton.isDown)
+			game.state.start('preloadMenuState');
 
 	}
 }
