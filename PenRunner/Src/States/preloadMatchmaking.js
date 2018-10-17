1d5e4ae3dc2dd584ab6ca5aad6d38c6ca9b77221
César Romero Albertos <43405217+CesarRomeroAlbertos@users.noreply.github.com>
2018-10-17 12:43:50 +0200

    Añadido texto loading a todas las escenas preload

--- PenRunner/Src/States/preloadMatchmaking.js
+++ PenRunner/Src/States/preloadMatchmaking.js
@@ -11,7 +11,8 @@ PenRunner.preloadMatchmakingState.prototype =
     	var text = "Loading...";
     	var style = { font: "65px Arial", fill: "#ffffff", align: "center" };
 
-    	var t = game.add.text(game.world.centerX-150, game.world.centerY-100, text, style);
+		var t = game.add.text(game.world.centerX-150, game.world.centerY-100, text, style);
+		
     	game.load.spritesheet('button', 'Assets/button_sprite_sheet.png', 193, 71);
         game.load.image('background','Assets/starfield.jpg');
 
--- PenRunner/Src/States/preloadTitle.js
+++ PenRunner/Src/States/preloadTitle.js
@@ -6,7 +6,10 @@ PenRunner.preloadTitleState.prototype =
 {
 	preload:function()
 	{
+		var text = "Loading...";
+    	var style = { font: "65px Arial", fill: "#ffffff", align: "center" };
 
+    	var t = game.add.text(game.world.centerX-150, game.world.centerY-100, text, style);
 	},
 	update: function()
 	{
