9c4fa184970f518e6b586a96906459c352c83f50
carlos91643 <32589575+carlos91643@users.noreply.github.com>
2018-10-28 23:19:23 +0100

    Cambios sistema puntuaciones
    He cambiado el script, ahora sale en primer lugar el jugador que ha ganado y la puntuacion que ha recibido en funcion de su llegada a la meta

--- PenRunner/Src/States/finalscore.js
+++ PenRunner/Src/States/finalscore.js
@@ -7,7 +7,7 @@ var buttonReturn;
 var jugador1, jugador2, jugador3, jugador4, jugador5, jugador6; //Variables asociadas a los sprites de fondo de cada uno de los jugadores
 var text = 'Puntuación Final';
 var style4 =  { font: "60px Arial", fill: "#ffffff", align: "center"};
-var puntuacion1, puntuacion2, puntuacion3, puntuacion4, puntuacion5, puntuacion6 = 0; //Variables que guardan la puntuacion de cada uno de los jugadores
+var puntuacion1 = 0, puntuacion2 = 0, puntuacion3, puntuacion4, puntuacion5, puntuacion6 = 0; //Variables que guardan la puntuacion de cada uno de los jugadores
 
 
 
@@ -26,9 +26,9 @@ PenRunner.scoreState.prototype =
 		buttonReturn.height = 50;
 
 		//De momento, las puntuaciones son estaticas, más adelante, se guardarán en función de la escena anterior
-		for(var i= 0; i < goalOrder; i+2)
-		{
-			if(goalOrder[i]==1)
+	//	for(var i= 0; i < 2; i++)
+		//{
+			if(goalOrder[0]==1)
 			{
 				puntuacion1+=10;
 				puntuacion2+=8;
@@ -38,7 +38,7 @@ PenRunner.scoreState.prototype =
 				puntuacion2+=10;
 				puntuacion1+=8;
 			}
-		}
+		//}
 
 		//Creamos los sprites para los fondos de puntuación de los jugadores
 		jugador = game.add.sprite(game.world.x+100, game.world.y+150, 'jugador');
@@ -49,8 +49,8 @@ PenRunner.scoreState.prototype =
 		jugador6 = game.add.sprite(game.world.x+100, game.world.y+500, 'jugador');
 		
 		//Ponemos el texto de la puntuacion en el fondo correspondiente.
-		puntuacion1 = game.add.text(game.world.x+120, game.world.y+165, '1. Jugador 1 .......................................... ' + puntuacion1 + ' pts');
-		puntuacion2 = game.add.text(game.world.x+120, game.world.y+235, '2. Jugador 2 ............................................ ' + puntuacion2 + ' pts');
+		game.add.text(game.world.x+120, game.world.y+165, '1. Jugador ' + goalOrder[0] + '.......................................... ' + puntuacion1 + ' pts');
+		game.add.text(game.world.x+120, game.world.y+235, '2. Jugador ' + goalOrder[1] + '............................................ ' + puntuacion2 + ' pts');
 
 		//Se escala el fondo de puntuacion de los jugadores, a priori es un sprite muy básico, se cambiará mas adelante
 	 	jugador.scale.setTo(1.2, 0.5);
